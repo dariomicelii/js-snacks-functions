4bfcc953384a35f20bc7859c1694a9bdb69443df
dariomicelii <dariomiceli01@gmail.com>
2024-10-21 17:06:25 +0200

    conta vocali

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,9 +1,38 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
-
+//! Funzione classica
+const word1 = 'javascript';
+vocaliList = ["a", "e", "i", "o", "u"];
+newArray = [];
 // Dichiara la funzione qui.
+function vocali (word) {
+for (let i = 0; i < word.length; i++){
+    if (vocaliList.includes(word[i])){
+        newArray.push(word[i])
+    }
+}
+console.log(newArray.length)
+return newArray.length
+}
+
+vocali(word)
+
+//! Arrow function (?)
+const word2 = 'javascript';
+vocaliList = ["a", "e", "i", "o", "u"];
+newArray = [];
+// Dichiara la funzione qui.
+let vocali = (word) => {
+for (let i = 0; i < word.length; i++){
+    if (vocaliList.includes(word[i])){
+        newArray.push(word[i])
+    }
+}
+console.log(newArray.length)
+return newArray.length
+}
+
+vocali(word)
 
 
 // Invoca la funzione qui e stampa il risultato in console
@@ -11,3 +40,5 @@ const word = 'javascript';
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+
+
