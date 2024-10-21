53b62febbe33052b91ac6f336ddda63c5f57f8f8
dariomicelii <dariomiceli01@gmail.com>
2024-10-21 17:17:14 +0200

    Saluti

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,9 +1,11 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const name = 'Mario';
-
+const name = 'Dario';
+let message = "";
 
 // Dichiara la funzione qui.
+let saluti = (name) => console.log(message = `Ciao ${name}`) 
+saluti(name)
 
 
 // Invoca la funzione qui e stampa il risultato in console
