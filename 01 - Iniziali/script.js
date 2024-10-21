e0a278f151f28e078ce7a5b3c96425f2ae23ac5b
dariomicelii <dariomiceli01@gmail.com>
2024-10-21 17:35:00 +0200

    iniziali (fatto male)

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -1,13 +1,22 @@
-/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo 
+un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-
+console.log(names)
 
 // Dichiara la funzione qui.
 
+function iniziali(names) {
+let newArray = [];
+cont = 0
+while(cont < names.length) {
+    currentName = names[i];
+    console.log(currentName)
+}
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+iniziali(names)
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
