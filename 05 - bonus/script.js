14b894a949c68f5582df5c64745fb46a8ef52323
dariomicelii <dariomiceli01@gmail.com>
2024-10-22 10:48:02 +0200

    bonus (correzione)

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -9,10 +9,21 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
+const greet = (name) => {
+    const now = new Date()
+    const currentHours = now.getHours();
+    
+    let greet;
+    if(currentHours <= 13){
+        greet = 'Buongiorno'
+    }else if(currentHours <= 17){
+        greet = 'Buon pomeriggio'
+    }else{
+        greet = 'Buonasera'
+    }
 
-
+    return `${greet} ${name}`
+}
 // Invoca la funzione qui e stampa il risultato in console
-
-
-
+console.log(greet("Dario"))
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
